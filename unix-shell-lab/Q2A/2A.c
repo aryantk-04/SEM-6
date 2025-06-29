eefa1b34eda9a8f3e67d47b22f127a09cb39b852
aryantk-04 <aryantk2468@outlook.com>
2025-06-29 10:01:11 +0530

    Added more statistics

--- unix-shell-lab/Q2A/2A.c
+++ unix-shell-lab/Q2A/2A.c
@@ -1,26 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
+
+        struct stat st;
 
         if (argc < 2) {
                 printf("Usage %s <filename>\n", argv[0]);
                 return 0;
         }
+        if ((stat(argv[1], &st)) == -1) {
+               printf("File cannot be opened\n\n");
+               return 0;
+        }
 
-        int fd = open(argv[1], O_RDWR);
-
-        struct stat st;
-        stat(argv[1], &st);
-
-        printf("5 fields are: \n");
-        printf("Mode:\t\t\t%d\n", st.st_mode & 0777);
+        printf("File statistics are:\n\n");
+        printf("Mode:\t\t\t%o\n", st.st_mode &07777);
         printf("Inode:\t\t\t%ld\n", st.st_ino);
-        printf("Size:\t\t\t%ld\n", st.st_size);
+        printf("Number of links:\t%ld\n", st.st_nlink);
         printf("Block Size:\t\t%ld\n", st.st_blksize);
-        printf("Number of Links:\t%ld\n", st.st_nlink);
+        printf("Size:\t\t\t%ld\n", st.st_size);
+        printf("User ID:\t\t%d\n", st.st_uid);
+        printf("Group ID:\t\t%d\n", st.st_gid);
+        printf("Last Modified Time:\t%s\n", ctime(&st.st_mtime));
+        printf("Access Time:\t\t%s\n", ctime(&st.st_atime));
 
         return 0;
+
 }
