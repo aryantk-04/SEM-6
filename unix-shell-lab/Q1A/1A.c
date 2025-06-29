05290b89dbbea7450dabc97ab8b24f8bc5f145d6
aryantk-04 <aryantk2468@outlook.com>
2025-06-29 16:10:48 +0530

    All errors fixed

--- unix-shell-lab/Q1A/1A.c
+++ unix-shell-lab/Q1A/1A.c
@@ -3,24 +3,39 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
 
+        if (argc < 2) {
+                printf("Usage %s <filename>\n", argv[0]);
+                return 0;
+        }
+
+        int fd = open(argv[1], O_RDWR);
+
         struct stat st;
+        stat(argv[1], &st);
 
-        if (argc < 3) {
-                printf("Usage %s <src> <dest>\n", argv[0]);
-                return 0;
+        int n = st.st_size;
+        printf("Length of file in bytes is %d\n", n);
+
+        char buf[2];
+
+        int pos = lseek(fd, 0, SEEK_END);
+
+        while (--pos >= 0) {
+
+                lseek(fd, pos, SEEK_SET);
+                read(fd, buf, 1);
+                buf[1] = '\0';
+                printf("%s", buf);
+
         }
 
-        int src = open(argv[1], O_RDONLY);
-        if (src == -1) {
-                printf("%s doesn't exist\n", argv[1]);
-                return 0;
-        }
+        printf("\n");
 
-        int dest = open(argv[2], O_CREAT | O_WRONLY, 0777);
-        if (dest == -1) {
-                printf("%s doesn't exist\n", argv[1]);
-                return 0;
-       
+        return 0;
+
+}
